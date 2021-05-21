faff1ac4042a95dbc644992b9dd117b0d31de75b
지예성 <@DESKTOP-EFDDBC1>
2021-05-21 14:11:04 +0900

    java_teamproject

--- Teamproject/src/tp/Plant.java
+++ Teamproject/src/tp/Plant.java
@@ -1,9 +1,20 @@
 package tp;
 
-public class Plant {
+public class Plant{
 
 	public Plant() {
 		
 	}
+	private int hp;
+	private int rate;	//게임종료시 남아있는 hp를 받아와 점수 매기기
+	
+	public int getrate() {	//식물의 hp를 받아와서 성공률 보여주기
+		return rate;
+	}
+	
+	public void setHP() {	//work 검사 후 +- 10씩
+		this.hp = hp;
+	}
+	
 	
 }
--- Teamproject/src/tp/runGame.java
+++ Teamproject/src/tp/runGame.java
@@ -7,6 +7,7 @@ public class runGame {
 		while(!isFinish()) {
 			
 		}
+		print
 	}
 
 }
