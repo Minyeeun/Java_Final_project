aad361108435cda60d1ea059e7f3cd9ba69fcc9d
Sunwoo Kim <sunkim09@g.skku.edu>
2021-05-29 21:39:50 +0900

    0529 쓰레드 실행용 서버 클래스

--- java_FarmGame/src/farmGame/ServerMain.java
+++ java_FarmGame/src/farmGame/ServerMain.java
@@ -6,38 +6,38 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.Date;
 
-/* 소켓통신을 이용하여 각 클라이언트로부터 점수 받아오는 클래스 */
+/* ServerThread 실행하는 클래스  */
 public class ServerMain {
 	
-	public static void main(String[] args) {
-		
-		int allScore[] = null;
-		int num = 0;
+	public void go() throws IOException {
+	
+		ServerSocket ss = null;
+		Socket s = null;
 		
 		try {
-			new Thread(new SocketConnect()).start(); // SocketConnect 쓰레드 시작   
-			ServerSocket serverSocket = new ServerSocket(8000); // 서버 소켓 생성   
+			ss = new ServerSocket(8000); // 서버 소켓 선언 
 			
-			Socket serversocket = serverSocket.accept(); // SocketConnect 소켓 받기  
-			
-			DataInputStream in = new DataInputStream(serversocket.getInputStream()); // 클라이언트에서 받음  
-			DataOutputStream out = new DataOutputStream(serversocket.getOutputStream()); // 클라이언트에 전달   
-			// 인원 정해지면 범위 바꾸기  
-			while(true) {
+			while (true) {
 				
-				allScore[num] = in.readInt(); // 클라이언트 score 값 저장   
-				++num;
+				s = ss.accept(); // 클라이언트 소켓 생성 
+				ServerThread st = new ServerThread(s); // 클라이언트 소켓으로 쓰레기 실행 
+				st.start();
 				
 			}
-			
-			// 점수판 띄우고 클라이언트에 보내기   
-		
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} finally {
+			try {
+				if (s != null)  s.close();
+				if (ss != null) ss.close();
+			} catch (Exception e) {e.printStackTrace();}
 		}
+	}
+	
+	
+	public static void main(String[] args) {
+		ServerMain sm = new ServerMain();
+		
 	}
 }
