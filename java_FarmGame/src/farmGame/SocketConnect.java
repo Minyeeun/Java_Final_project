de24b88a306aabfc781a4988f55ae949d75a123c
Sunwoo Kim <sunkim09@g.skku.edu>
2021-05-29 21:40:43 +0900

    0529 사용자 점수 받아오고 서버로 점수 보내는 클래스

--- java_FarmGame/src/farmGame/SocketConnect.java
+++ java_FarmGame/src/farmGame/SocketConnect.java
@@ -1,30 +1,30 @@
 package farmGame;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
-/* FarmPanel 에서 최종점수 받아오는 + 최종 점수판 보여주는 클라이언트 소켓 */
-public class SocketConnect implements Runnable{
-	DataOutputStream toServer = null; // 서버에 전달  
-	DataInputStream fromServer = null;  // 서버에서 받음  
+/* FarmPanel 에서 최종점수 받아오는 클래스 */
+public class SocketConnect {
+	Socket socket = null; // 소켓 선언  
+	DataOutputStream toServer = null; // ServerThread 로 전달하기 위해  
 	
-	/* 쓰레드 */
-	public void run() {
+	private static SocketConnect instance = new SocketConnect();
+	
+	public SocketConnect() {
 		try {
-			Socket socket = new Socket("localhost", 8000);
-			toServer = new DataOutputStream(socket.getOutputStream());
-			fromServer = new DataInputStream(socket.getInputStream());
-			
-			/***************************************************/
-			int score = result; // FarmPanel 에서 받아온 최종점수 
-			toServer.writeInt(score); // 서버로 점수 전달   
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			socket = new Socket(); 
+		} catch (Exception e) {}
+	}
+	
+	/* 인스턴트 받기 */
+	public static SocketConnect getInstance() {
+		return instance;
+	}
+	
+	/* 최종 점수 저장하기 */
+	public void SaveRank(int sc) throws IOException{
+		toServer = new DataOutputStream(socket.getOutputStream());
+		toServer.writeInt(sc); // 서버로 점수 보내기 
 	}
 }
